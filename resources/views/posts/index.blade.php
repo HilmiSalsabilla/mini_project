14541bf10cae535c3410ce18529e8cb14112d169
Hilmi Salsabilla <hilmisalsabilla6@gmail.com>
2025-07-31 20:43:47 +0700

    edit view

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,7 +9,7 @@
         <p>Debug User: {{ auth()->user() ? auth()->user()->name : 'Guest' }}</p> --}}
         @forelse ($posts as $post)
             <div class="card rounded-xl mb-3">
-                <div class="card-body rounded-xl hover:shadow-lg p-4">
+                <div class="card-body rounded-xl hover:shadow-xl p-4">
                     <h5 class="font-bold mb-3"> {{ $post->title }} </h5>
                     <hr class="divide-solid mb-3">
                     <p> {!! nl2br(e($post->content)) !!} </p>
